fe9adbe9f95e5c2dcd3ac807a8cd52ab8eaebc1c
Varvara Nechaeva <85786864+VarvaraNechaeva@users.noreply.github.com>
2021-06-27 15:03:10 +0200

    initial implementation of Character class

---
+++ src/Character.java
@@ -0,0 +1,37 @@
+import java.util.UUID;
+
+abstract class Character {
+  UUID id;
+  String Name;
+  int hp;
+
+  public Character(String name, int hp) {
+    this.id = UUID.randomUUID();
+    Name = name;
+    this.hp = hp;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public String getName() {
+    return Name;
+  }
+
+  public void setName(String name) {
+    Name = name;
+  }
+
+  public int getHp() {
+    return hp;
+  }
+
+  public void setHp(int hp) {
+    this.hp = hp;
+  }
+
+  public boolean isAlive() {
+    return hp > 0;
+  }
+}
